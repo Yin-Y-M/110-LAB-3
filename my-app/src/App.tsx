75ae28695035b882c0adc90e99e401a7bcfdf71b
Yin-Y-M <117802747+Yin-Y-M@users.noreply.github.com>
2024-10-14 15:03:03 -0700

    Initial Push

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useContext } from 'react';
+import './App.css';
+import { StickyNotes } from './stickyNotes';
+
+function App() {
+    return (
+        <div>
+            <StickyNotes />
+        </div>
+    );
+}
+
+export default App ;
